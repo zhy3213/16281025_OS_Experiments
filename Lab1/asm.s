478289bc470e2a3c3c7ef1485cb731a0ee040a74
Haoyang <haoyang_zhang@hotmail.com>
2019-03-12 09:48:19 +0800

    Add source code and excutables

---
+++ Lab1/asm.s
@@ -0,0 +1,29 @@
+	.file	"asmprintf.c"
+	.section	.rodata
+.LC0:
+	.string	"Hello World"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB2:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movq	%rsi, -16(%rbp)
+	leaq	.LC0(%rip), %rdi
+	call	puts@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 6.3.0-12ubuntu2) 6.3.0 20170406"
+	.section	.note.GNU-stack,"",@progbits
